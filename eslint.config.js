359ae6a5696ca90760f2c9c25df2d8ed96607430
rimapaulione <rimapaulione@gmail.com>
2025-03-08 10:39:03 +0200

    Fixed linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,40 +1,64 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-plugin-prettier';
+import configPrettier from 'eslint-config-prettier';
 
 export default [
-  { ignores: ['dist'] },
+  {
+    ignores: ['dist'],
+  },
+
   {
     files: ['**/*.{js,jsx}'],
+
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.vitest,
+      },
       parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
+
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      prettier: 'eslint-plugin-prettier',
+      prettier,
     },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:prettier/recommended',
-    ],
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
     rules: {
       ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      ...prettier.configs.recommended.rules,
+
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'react/prop-types': 'off',
       'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      'react/react-in-jsx-scope': 'off',
     },
   },
+
+  configPrettier,
 ];
